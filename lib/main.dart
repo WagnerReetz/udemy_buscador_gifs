429b9d7a56cc790e77a2d755d464fd46eb335580
Wagner.Reetz <wagner.reetz@senior.com.br>
2021-12-29 16:04:22 -0300

    projeto final

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
+import 'package:buscador_gifs/ui/gif_page.dart';
+import 'package:buscador_gifs/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.white),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          hintStyle: TextStyle(color: Colors.white),
+        )),
   ));
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
